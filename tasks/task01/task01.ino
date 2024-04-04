ef5a792b20f74257f8ac0a11880ce861185b3200
tarenkovia <tarenkov_ivan_00@mail.ru>
2024-04-04 22:00:23 +0400

    added 1 and 2 tasks

--- tasks/task01/task01.ino
+++ tasks/task01/task01.ino
@@ -1,7 +1,97 @@
-void setup() {
+long* intervalMicros = new long[5];
+long intervals[5] = {10000, 1000, 500, 100, 50};
+int* lampStates = new int[5];
+int pins[5] = {3, 5, 6, 9, 10};
 
+void setup() {
+  for (int i = 0; i < 5; i++) 
+  {
+    intervalMicros[i] = 0;
+    lampStates[i] = LOW;
+    pinMode(pins[i], OUTPUT);
+  }
 }
 
 void loop() {
+  unsigned long micrCur = micros();
 
+  if (micrCur - intervalMicros[0] >= intervals[0]) 
+  {
+    intervalMicros[0] = micrCur;
+
+    if (lampStates[0] == LOW) 
+    {
+      lampStates[0] = HIGH;
+    }
+    else 
+    {
+      lampStates[0] = LOW;
+    }
+
+    digitalWrite(pins[0], lampStates[0]);
+  }
+
+  if (micrCur - intervalMicros[1] >= intervals[1]) 
+  {
+    intervalMicros[1] = micrCur;
+
+    if (lampStates[1] == LOW) 
+    {
+      lampStates[1] = HIGH;
+    }
+    else 
+    {
+      lampStates[1] = LOW;
+    }
+
+    digitalWrite(pins[1], lampStates[1]);
+  }
+  
+  if (micrCur - intervalMicros[2] >= intervals[2]) 
+  {
+    intervalMicros[2] = micrCur;
+
+    if (lampStates[2] == LOW) 
+    {
+      lampStates[2] = HIGH;
+    }
+    else 
+    {
+      lampStates[2] = LOW;
+    }
+
+    digitalWrite(pins[2], lampStates[2]);
+  }
+
+  if (micrCur - intervalMicros[3] >= intervals[3]) 
+  {
+    intervalMicros[3] = micrCur;
+
+    if (lampStates[3] == LOW)
+    {
+      lampStates[3] = HIGH;
+    }
+    else 
+    {
+      lampStates[3] = LOW;
+    }
+
+    digitalWrite(pins[3], lampStates[3]);
+  }
+
+  if (micrCur - intervalMicros[4] >= intervals[4]) 
+  {
+    intervalMicros[4] = micrCur;
+
+    if (lampStates[4] == LOW) 
+    {
+      lampStates[4] = HIGH;
+    }
+    else 
+    {
+      lampStates[4] = LOW;
+    }
+
+    digitalWrite(pins[4], lampStates[4]);
+  }
 }
--- tasks/task02/task02.ino
+++ tasks/task02/task02.ino
@@ -1,7 +1,77 @@
-void setup() {
+#include <TaskManagerIO.h>
 
+long intervals[5] = {10000, 1000, 500, 100, 50};
+int* states = new int[5];
+int pins[5] = {3, 5, 6, 9, 10};
+
+void setup() {
+  for (int i = 0; i < 5; i++) 
+  {
+    states[i] = LOW;
+    pinMode(pins[i], OUTPUT);
+  }
+
+  taskid_t taskId0 = taskManager.scheduleFixedRate(intervals[0], [] {
+    if (states[0] == LOW) 
+    {
+      states[0] = HIGH;
+    }
+    else {
+      states[0] = LOW;
+    }
+
+    digitalWrite(pins[0], states[0]);
+  }, TIME_MICROS);
+
+  taskid_t taskId1 = taskManager.scheduleFixedRate(intervals[1], [] {
+    if (states[1] == LOW) 
+    {
+      states[1] = HIGH;
+    }
+    else {
+      states[1] = LOW;
+    }
+
+    digitalWrite(pins[1], states[1]);
+  }, TIME_MICROS);
+
+  taskid_t taskId2 = taskManager.scheduleFixedRate(intervals[2], [] {
+    if (states[2] == LOW) 
+    {
+      states[2] = HIGH;
+    }
+    else {
+      states[2] = LOW;
+    }
+
+    digitalWrite(pins[2], states[2]);
+  }, TIME_MICROS);
+
+  taskid_t taskId3 = taskManager.scheduleFixedRate(intervals[3], [] {
+    if (states[3] == LOW) 
+    {
+      states[3] = HIGH;
+    }
+    else {
+      states[3] = LOW;
+    }
+
+    digitalWrite(pins[3], states[3]);
+  }, TIME_MICROS);
+
+  taskid_t taskId4 = taskManager.scheduleFixedRate(intervals[4], [] {
+    if (states[4] == LOW) 
+    {
+      states[4] = HIGH;
+    }
+    else {
+      states[4] = LOW;
+    }
+
+    digitalWrite(pins[4], states[4]);
+  }, TIME_MICROS);
 }
 
 void loop() {
-
+  taskManager.runLoop();
 }
